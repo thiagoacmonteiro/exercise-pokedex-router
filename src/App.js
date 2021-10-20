2be1bd72242f34a6b2e2d1e5bc5c804d018ce9bb
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-10-19 22:07:06 -0300

    Exercise 1 solved

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      {/* Exercise 1: After loading display the pokedex */}
+      <BrowserRouter>  
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons} />
+      </BrowserRouter>
     </div>
   );
 }
